5a310f9f3e3f17b9770a7ceb5c48f8a091f064b9
SebastianIslas <sebasislasbecerra@gmail.com>
2023-10-16 22:06:39 -0600

    Ajustes

--- proyecto4.py
+++ proyecto4.py
@@ -8,12 +8,6 @@ from sklearn.cluster import KMeans  # Importa KMeans
 
 
 def crear_lista_reproduccion(nombre_archivo, canciones):
-    """
-    Crea un archivo de lista de reproducción M3U.
-
-    :param nombre_archivo: Nombre del archivo M3U (por ejemplo, "mi_lista.m3u").
-    :param canciones: Una lista de rutas de archivos de canciones a incluir en la lista de reproducción.
-    """
     try:
         with open(nombre_archivo, 'w') as archivo_m3u:
             # Escribir la cabecera del archivo M3U
@@ -30,21 +24,16 @@ def crear_lista_reproduccion(nombre_archivo, canciones):
 
 
 
-
-
-# Ruta al archivo de audio (reemplaza con tu propia ruta)
-archivo_audio = "darkSide.mp3"  # Cambia a tu archivo WAV o MP3
-
-# Definir la ruta de la carpeta con los archivos de audio
+#Ruta de la carpeta con los archivos de audio
 carpeta_audio = "./"
 
 # Obtener una lista de todos los archivos en la carpeta
 archivos = os.listdir(carpeta_audio)
 
-# Filtrar la lista para solo mantener archivos MP3 y WAV
+# Filtrar la lista para solo mantener archivos MP3, WAV y FLAC
 archivos_audio = [archivo for archivo in archivos if archivo.endswith('.mp3') or archivo.endswith('.wav') or archivo.endswith('.flac')]
 
-
+#Creamos 1 array para cada clasificacion
 canciones1 = []
 canciones2 = []
 
@@ -64,9 +53,9 @@ for archivo_audio in archivos_audio:
     # Calcular los decibelios (dB)
     dB = audio.dBFS
 
-
     # Obtener la forma de onda del audio
     forma_onda = np.array(audio.get_array_of_samples())
+
     # Convertir el array de audio a punto flotante entre -1 y 1
     forma_onda_bpm = forma_onda / 2**15  # Si tu audio es de 16 bits
 
@@ -74,10 +63,6 @@ for archivo_audio in archivos_audio:
     promedio = np.mean(forma_onda)
     # Calcular el tempo (BPM) de la canción
     tempo, _ = librosa.beat.beat_track(y=forma_onda_bpm, sr=frecuencia_hz)
-
-    # Crear un arreglo de tiempo para la gráfica de la forma de onda
-    #t = np.linspace(0, duracion_ms / 1000, duracion_ms)
-    t = np.linspace(0, duracion_ms / 1000, len(forma_onda))
 
     # Mostrar información del audio
     print(f"Duración: {duracion_ms} ms")
@@ -91,7 +76,41 @@ for archivo_audio in archivos_audio:
     else:
         canciones2.append(os.path.join(carpeta_audio, archivo_audio))
 
+    ########### GRAFICAR
+    ''''''
+    # Crear un arreglo de tiempo para la gráfica de la forma de onda
+    #t = np.linspace(0, duracion_ms / 1000, duracion_ms)
+    t = np.linspace(0, duracion_ms / 1000, len(forma_onda))
+    # Crear una gráfica de la forma de onda
+    plt.figure(figsize=(10, 4))
+    plt.plot(t, forma_onda)
+    plt.title("Forma de Onda del Audio")
+    plt.xlabel("Tiempo (s)")
+    plt.ylabel("Amplitud")
+    plt.grid(True)
+
+    # Mostrar la gráfica
+    plt.show()
+    ''''''
+
+
 
 # Ejemplo de uso:
 crear_lista_reproduccion("clasif1.m3u", canciones1)
 crear_lista_reproduccion("clasif2.m3u", canciones2)
+
+
+def intentoClasificacion():
+    # Clasificación básica de sentimiento basada en tempo
+    X = np.array([dB]).reshape(-1, 1)  # Añade esta línea para ajustar la forma de X
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+    kmeans = KMeans(n_clusters=1, n_init=10)  # Puedes ajustar el número de clusters
+    kmeans.fit(X)
+    labels = kmeans.predict(X)
+
+    # Mostrar el sentimiento basado en el cluster
+    sentimientos = {0: "Feliz", 1: "Triste", 2: "Neutro"}  # Puedes ajustar las etiquetas
+    sentimiento_predicho = sentimientos[np.argmax(np.bincount(labels))]
+
+    print(f"Sentimiento: {sentimiento_predicho}")
